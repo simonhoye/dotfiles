cd2f565a690ef454390129d8a9aa2f3fec785ab8
Simon Hoye <simonhoye@gmail.com>
2020-05-19 20:26:18 +1000

    adds 1password installation

--- os/install/macos/misc.sh
+++ os/install/macos/misc.sh
@@ -8,6 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n Miscellaneous\n\n"
 
+brew_install "1Password" "1password" "homebrew/cask" "cask"
 brew_install "Joplin" "joplin" "homebrew/cask" "cask"
 brew_install "saml2aws" "saml2aws" "versent/homebrew-taps"
 brew_install "Serverless Framework" "serverless"
