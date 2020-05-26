4a8eb91a01fff1532b320c4ef8065a7c5906354b
Simon Hoye <simonhoye@gmail.com>
2020-05-26 21:01:17 +1000

    adds LICEcap installation

--- os/install/macos/misc.sh
+++ os/install/macos/misc.sh
@@ -9,7 +9,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n Miscellaneous\n\n"
 
 brew_install "1Password" "1password" "homebrew/cask" "cask"
+brew_install "Docker" "docker" "homebrew/cask" "cask"
 brew_install "Joplin" "joplin" "homebrew/cask" "cask"
+brew_install "LICEcap" "licecap" "homebrew.cask" "cask"
 brew_install "saml2aws" "saml2aws" "versent/homebrew-taps"
 brew_install "Serverless Framework" "serverless"
+brew_install "Virtual Box" "virtualbox" "homebrew/cask" "cask"
 brew_install "Zoom"  "zoomus" "homebrew/cask" "cask"
